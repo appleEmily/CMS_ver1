769e1b1d88ea2067065b3ff8b8dd870cceaae893
appleEmily <emiri3128@gmail.com>
2020-01-16 21:35:59 +0900

    Initial Commit

---
+++ CMS_ver1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CMS_ver1
+//
+//  Created by 野崎絵未里 on 2020/01/15.
+//  Copyright © 2020 emily.com. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ CMS_ver1Tests/CMS_ver1Tests.swift
@@ -0,0 +1,34 @@
+//
+//  CMS_ver1Tests.swift
+//  CMS_ver1Tests
+//
+//  Created by 野崎絵未里 on 2020/01/15.
+//  Copyright © 2020 emily.com. All rights reserved.
+//
+
+import XCTest
+@testable import CMS_ver1
+
+class CMS_ver1Tests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
