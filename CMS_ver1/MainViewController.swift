2097fcbece6c9a1fb460661d7ff89d39b6bd17bb
appleEmily <emiri3128@gmail.com>
2020-01-16 23:56:02 +0900

    main pages アイコン4つにできた

---
+++ CMS_ver1/MainViewController.swift
@@ -0,0 +1,23 @@
+//
+//  MainViewController.swift
+//  CMS_ver1
+//
+//  Created by 野崎絵未里 on 2020/01/16.
+//  Copyright © 2020 emily.com. All rights reserved.
+//
+
+import UIKit
+
+class MainViewController: UIViewController,UITabBarDelegate {
+
+    @IBOutlet weak var tabBar: UITabBar!
+    @IBOutlet weak var GoHome: UITabBarItem!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+}
